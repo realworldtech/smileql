05967d5c05c391fd13c01392a0b79248a028d6d3
Karl Kloppenborg <k@rl.ag>
2019-06-21 14:25:39 +1000

    set version start

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="Inomial Smile GraphQL Interface",
-    version="0.3",
+    version="1.0.0",
     classifiers=[
         "Intended Audience :: Inomial Customers",
         "Programming Language :: Python :: 3.7",
